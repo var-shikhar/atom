1ad84d56dce46aef70f24ebfef2dd2832378e56c
Shikhar Varshney <shikharvarshney10@gmail.com>
2024-09-25 15:03:53 +0530

    update

--- frontend/src/util/routes.js
+++ frontend/src/util/routes.js
@@ -2,25 +2,25 @@ const BACKEND_ROUTES = 'https://atom-7y9m.onrender.com';
 // const BACKEND_ROUTES = 'http://localhost:8081';
 
 
-const loginRoute = `${BACKEND_ROUTES}/admin/auth/login`;
-const logoutRoute = `${BACKEND_ROUTES}/admin/auth/logout`;
+const loginRoute = `${BACKEND_ROUTES}/admin/admin/auth/login`;
+const logoutRoute = `${BACKEND_ROUTES}/admin/admin/auth/logout`;
 
-const commonCategoryRoute = `${BACKEND_ROUTES}/admin/category`;
-const commonSubCategoryRoute = `${BACKEND_ROUTES}/admin/subCategory`;
+const commonCategoryRoute = `${BACKEND_ROUTES}/admin/admin/category`;
+const commonSubCategoryRoute = `${BACKEND_ROUTES}/admin/admin/subCategory`;
 
-const commonProductRoute = `${BACKEND_ROUTES}/admin/product`;
-const commonProductVariationRoute = `${BACKEND_ROUTES}/admin/product/variation`;
+const commonProductRoute = `${BACKEND_ROUTES}/admin/admin/product`;
+const commonProductVariationRoute = `${BACKEND_ROUTES}/admin/admin/product/variation`;
 
-const commonOrderRoute = `${BACKEND_ROUTES}/admin/order`;
-const commonOrderDetailRoute = `${BACKEND_ROUTES}/admin/order/detail`;
-const commonOrderStatusRoute = `${BACKEND_ROUTES}/admin/order/status`;
+const commonOrderRoute = `${BACKEND_ROUTES}/admin/admin/order`;
+const commonOrderDetailRoute = `${BACKEND_ROUTES}/admin/admin/order/detail`;
+const commonOrderStatusRoute = `${BACKEND_ROUTES}/admin/admin/order/status`;
 
-const commonCustomerRoute = `${BACKEND_ROUTES}/admin/customer`;
-const commonCustomerDetailRoute = `${BACKEND_ROUTES}/admin/customer/detail`;
+const commonCustomerRoute = `${BACKEND_ROUTES}/admin/admin/customer`;
+const commonCustomerDetailRoute = `${BACKEND_ROUTES}/admin/admin/customer/detail`;
 
-const commonCouponRoute = `${BACKEND_ROUTES}/admin/coupon`;
+const commonCouponRoute = `${BACKEND_ROUTES}/admin/admin/coupon`;
 
-const commonContactLeadRoute = `${BACKEND_ROUTES}/admin/contact-lead`;
+const commonContactLeadRoute = `${BACKEND_ROUTES}/admin/admin/contact-lead`;
 
 
 
